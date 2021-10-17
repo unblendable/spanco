e91ef5d0f6ab3b717407fa00d409be34d3413546
unmergable <melloretsu@yahoo.com>
2021-10-18 01:05:14 +0700

    add http

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = new express();
 const mysql = require('mysql');
 const path = require('path');
+const http = require('http');
+const https = require('https');
 const bodyParser = require('body-parser');
 const db = mysql.createConnection({   
     host: 'mysql-5.5.chaiyohosting.com',
@@ -11,7 +13,6 @@ const db = mysql.createConnection({
     database: 'span_db'
 })
 db.connect()
-global.mysql = db
 
 app.use(bodyParser.raw());
 app.use(bodyParser.json({ limit: "100mb", parameterLimit: 1000000 }));
@@ -39,6 +40,25 @@ app.use(function(req, res, next) {
     next();
 });
 require('./src/api')(app);
-app.listen('80', () => {    
-    console.log('start port 80')
-})
+
+var server = http.createServer(app);
+
+server.on('clientError', (err, socket) => {
+    socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+var port = 80
+server.on('error',   (e)  =>  {   
+    if  (e.code  ===  'EADDRINUSE')  {     
+        console.log('Address in use, retrying...');     
+        setTimeout(()  =>  {       
+            server.close();       
+            server.listen(port);     
+        },  1000);   
+    } 
+});
+
+server.listen(port,  function ()  {
+    global.mysql = db
+    console.log(`Webserver is ready and listening on port ${port}`);
+    console.log('opened server on',  server.address());
+});
